1df36fdc33e3a7f93e18a8d1f89a2d2d494e97a3
Prashant Varma <prashantvarma5083@gmail.com>
2024-09-05 10:11:12 +0530

    fix: token minting

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,34 @@ import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton,
-    WalletConnectButton
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+  WalletConnectButton
 } from '@solana/wallet-adapter-react-ui';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
 import Airdrop from './Airdrop';
-import CreateTokenMint from './CreateTokenMint';
+import { CreateTokenMint } from './CreateTokenMint';
 import { SignMessage } from './SignMessage';
+import { useMemo } from 'react';
 
 function App() {
-
+  const network = WalletAdapterNetwork.Devnet
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
   return (
     <>
-             <ConnectionProvider endpoint={'https://solana-devnet.g.alchemy.com/v2/yV8uf_2Ss1pPDEVsNL36hiOKqv7ctRH3'}>
-            <WalletProvider wallets={[]} autoConnect>
-                <WalletModalProvider>
-                    <WalletMultiButton />
-                    <WalletDisconnectButton />
-                    <Airdrop />
-                    <CreateTokenMint />
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+            <Airdrop />
+            <CreateTokenMint />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </>
   )
 }
